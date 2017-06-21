e9d26f6b427bfc892ec517d935723c7a0eb5e7b9
CIPL0498 <CIPL0498@CIPL0498JAVA.colanonline.local>
2017-06-21 17:17:00 +0530

    ZkEvaluation Configuarion has been changed at pom.xml and application.java

--- src/main/java/com/zk/Application.java
+++ src/main/java/com/zk/Application.java
@@ -33,8 +33,8 @@ public class Application extends SpringBootServletInitializer{
 		SpringApplication.run(applicationClass, args);
 	}
 	
+	@Override
 	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
 		return application.sources(applicationClass);
-		
 	}
 }
