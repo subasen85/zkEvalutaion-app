05b155bf8885eeec7c0d30fb44cc5eca6c87bbe2
Sivashankar M C <CIPL0498@colanonline.local>
2017-07-04 10:47:35 +0530

    Script file commited in zkBackend

--- src/main/ddl/zk_ddl_script.sql
+++ src/main/ddl/zk_ddl_script.sql
@@ -1,23 +1,33 @@
-CREATE TABLE `employee_db`.`login_info` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(200) NOT NULL,
-  `password` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE DATABASE `employee_db`;
+
+USE `employee_db`;
+
+/*Table structure for table `employee_details` */
+DROP TABLE IF EXISTS `employee_details`;
+CREATE TABLE `employee_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_name` varchar(150) NOT NULL,
+  `designation` varchar(100) NOT NULL,
+  `salary` double NOT NULL,
+  `eamil_id` varchar(255) NOT NULL,
+  `mobile_no` varchar(200) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `created_date` date NOT NULL,
+  `version` int(11) NOT NULL,
+  `gender` int(1) NOT NULL,
+  `emptype` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) 
+ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE `employee_db`.`employee_details` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `employee_name` VARCHAR(150) NOT NULL,
-  `designation` VARCHAR(100) NOT NULL,
-  `salary` DOUBLE NOT NULL,
-  `eamil_id` VARCHAR(255) NOT NULL,
-  `mobile_no` INT NOT NULL,
-  `status` VARCHAR(45) NOT NULL,
-  `created_date` DATE NOT NULL,
-  `version` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+/*Table structure for table `login_info` */
+DROP TABLE IF EXISTS `login_info`;
+CREATE TABLE `login_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) 
+ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=u
 
